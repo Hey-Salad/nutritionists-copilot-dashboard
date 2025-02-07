a2f27269b2852614f89de8716df7864331a2419f
Peter Machona <chilu.machona@icloud.com>
2025-02-07 01:39:55 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qpqcaqjuymwxrjfkljgq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwcWNhcWp1eW13eHJqZmtsamdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1Nzk5OTMsImV4cCI6MjA0NjE1NTk5M30.5ngFWEeTWOVd42tlkNgEc4MZIQGK1jlxCxdv3gBEWfY';
-
+const supabaseUrl = ''.
+const supabaseAnonKey = '',
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
